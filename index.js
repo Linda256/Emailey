92542dcb3b6209e5edabcc38f2d9beae0e5bfe24
Linda Wang <lindawang256@gmail.com>
2018-10-22 15:16:03 -0700

    Changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
-	res.send('Good Afternoon, Linda')
+	res.send('Good Afternoon, Linda! Have a good day')
 });
 
 app.listen(PORT,()=>console.log(`Server is running at port ${PORT}`))
