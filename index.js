5471f0b327ed3a6f1d28dce92cd09b0bd2044f48
Linda Wang <lindawang256@gmail.com>
2018-10-27 16:23:38 -0700

    test heroku

--- index.js
+++ index.js
@@ -6,19 +6,27 @@ const keys = require('./config/keys')
 
 const app = express();
 
+app.get('/',(req,res)=>{
+	res.send('Good Afternoon, Linda! Have a good day')
+});
+
+
+// passport.use(new GoogleStrategy({clientID:keys.googleClientID,
+//   clientSecret: keys.googleClientSecret,
+//   callbackURL:'/auth/google/callback'
+//   }, (accessToken)=>{
+//     console.log(accessToken)
+
+//   }));
+
+// app.get('/auth/google',passport.authenticate('google',{
+//   scope:['profile','email']
+// })
+// );
+
+// app.get('/auth/google/callback',passport.authenticate('google')
+// );
+
 const PORT = process.env.PORT || 5000;
-
-// app.get('/',(req,res)=>{
-// 	res.send('Good Afternoon, Linda! Have a good day')
-// });
-
-
-passport.use(new GoogleStrategy({clientID:keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL:'/auth/google/callback'
-  }, (accessToken)=>{
-    console.log(accessToken)
-
-  }));
 
 app.listen(PORT,()=>console.log(`Server is running at port ${PORT}`))
