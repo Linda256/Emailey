b63f1a8fedd2743c2ddc9389a17c46bf184a88c5
Linda Wang <lindawang256@gmail.com>
2018-11-15 17:02:46 -0800

    Save user google record

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,11 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 
 const keys = require('../config/keys')
+//const User = require('../models/User.js')
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +15,17 @@ passport.use(
       callbackURL:'/auth/google/callback'
     },
     (accessToken,refreshToken,profile,done)=>{
-      console.log('accessToken',accessToken);
-      console.log('refreshToken',refreshToken);
-      console.log('profile', profile);
+        User.findOne({googleId:profile.id})
+          .then((existingUser) => {
+            if(existingUser){
+              //we already have a record with the given profile ID
+            } else {
+              //we don't have a user reord with this ID, make a new record
+              new User({ googleId: profile.id }).save()
+            }
+          })
+
+        ;
     }
   )
 );
