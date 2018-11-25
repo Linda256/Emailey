e00f6b9c3ec70f4263589d7c594ef293bdf9b196
Linda Wang <lindawang256@gmail.com>
2018-11-24 18:05:09 -0800

    Add session authentication

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const keys = require('./config/keys');
 require('./models/User');
@@ -9,6 +11,16 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI, ()=>console.log('MongoDB is connected'));
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30*24*60*60*1000,//last for 30 days
+    keys:[keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // app.get('/',(req,res)=>{
 // 	res.send('Happy Fall Linda! Have a good day')
