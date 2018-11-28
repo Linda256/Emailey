645b22935ac0ca39f513f592c82b240581f2cfe3
Linda Wang <lindawang256@gmail.com>
2018-11-27 17:35:30 -0800

    Refactor code uing async/await

--- services/passport.js
+++ services/passport.js
@@ -17,6 +17,32 @@ passport.deserializeUser((id,done)=>{
   })
 })
 
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID:keys.googleClientID,
+//       clientSecret: keys.googleClientSecret,
+//       callbackURL:'/auth/google/callback',
+//       proxy:true
+//     },
+//     (accessToken,refreshToken,profile,done)=>{
+//         User.findOne({googleId:profile.id})
+//           .then((existingUser) => {
+//             if(existingUser){
+//               //we already have a record with the given profile ID
+//               done(null,existingUser);
+//             } else {
+//               //we don't have a user reord with this ID, make a new record
+//               new User({ googleId: profile.id }).save()
+//                 .then(user=>done(null,user));
+//             }
+//           })
+
+//         ;
+//     }
+//   )
+// );
+
 passport.use(
   new GoogleStrategy(
     {
@@ -25,21 +51,20 @@ passport.use(
       callbackURL:'/auth/google/callback',
       proxy:true
     },
-    (accessToken,refreshToken,profile,done)=>{
-        User.findOne({googleId:profile.id})
-          .then((existingUser) => {
-            if(existingUser){
+
+    async (accessToken,refreshToken,profile,done)=>{
+        const existingUser = await User.findOne({googleId:profile.id});
+
+        if(existingUser){
               //we already have a record with the given profile ID
               done(null,existingUser);
-            } else {
+        } else {
               //we don't have a user reord with this ID, make a new record
-              new User({ googleId: profile.id }).save()
-                .then(user=>done(null,user));
+        const user = await new User({ googleId: profile.id }).save();
+          done(null,user);
             }
-          })
+          }
 
-        ;
-    }
   )
 );
 
