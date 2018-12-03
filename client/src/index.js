66679f1e8ddb0567054ff930c46fdb9b474fc3fc
Linda Wang <lindawang256@gmail.com>
2018-12-03 11:34:03 -0800

    Add head component

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,5 @@
+import 'materialize-css/dist/css/materialize.min.css'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
@@ -5,6 +7,8 @@ import {createStore, applyMiddleware} from 'redux'
 
 import App from './components/App';
 import reducers from './reducers';
+
+
 
 
 const store = createStore(()=> reducers,{},applyMiddleware());
