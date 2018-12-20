93c138871e8155562903702bf2e3309c1b0a263f
Linda Wang <lindawang256@gmail.com>
2018-12-20 13:50:42 -0800

    Add user feedback response

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -7,21 +7,34 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 
 module.exports = app =>{
- app.post('/api/surveys',requireLogin, requireCredits,(req,res) =>{
-  //is the user logged in
-  // does the user have enough credit for sending email
-  const { title, subject, body,recipients } = req.body;
-
-  const survey = new Survey({
-    title: title,
-    body: body,
-    subject: subject,
-    recipients: recipients.split(',').map(email=> ({email:email.trim()})),
-    _user:req.user.id,
-    dateSent:Date.now()
+  app.get('/api/surveys',(req,res)=>{
+    res.send('Thanks for voting')
   })
+   app.post('/api/surveys',requireLogin, requireCredits,async (req,res) =>{
+    //is the user logged in
+    // does the user have enough credit for sending email
+    const { title, subject, body,recipients } = req.body;
 
-  const mailer = new Mailer(survey,surveyTemplate(survey));
-  mailer.send();
- })
+    const survey = new Survey({
+      title: title,
+      body: body,
+      subject: subject,
+      recipients: recipients.split(',').map(email=> ({email:email.trim()})),
+      _user:req.user.id,
+      dateSent:Date.now()
+    })
+
+    const mailer = new Mailer(survey,surveyTemplate(survey));
+    try{
+       await mailer.send();
+       await survey.save();
+       req.user.credits -= 1;
+       const user = await req.user.save();
+
+       res.send(user);
+    } catch (err){
+      res.status(422).send(err);
+    }
+
+   })
 }
